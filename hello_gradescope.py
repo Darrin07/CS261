485502aa09b81ab34140e2f5cc6d445589050cdb
Darrin Miller <darrinmiller07@gmail.com>
2023-10-09 19:34:05 -0400

    Assignment 0

---
+++ hello_gradescope.py
@@ -0,0 +1,9 @@
+# Name: Darrin Miller
+# OSU Email: milledar@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: 0
+# Due Date: 10/9/2023
+# Description: testing gradescope to see if its functioning correctly.
+
+def gradescope_test():
+    return 42
